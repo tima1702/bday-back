1f74eb9079bf0c2cb168165bdc7f7abcecf7dcec
Timofey Kuznetsov <tima1702@gmail.com>
2020-02-05 14:19:18 +0300

    Added postgres db

---
+++ config/config.js
@@ -0,0 +1,23 @@
+require('dotenv');
+
+module.exports = {
+  'development': {
+    'username': process.env.POSTGRES_USER,
+    'password': process.env.POSTGRES_PASSWORD,
+    'database': process.env.POSTGRES_DB,
+    'host': process.env.POSTGRES_HOST,
+    'dialect': 'postgres',
+  },
+  'test': {
+    'username': process.env.POSTGRES_USER,
+    'password': process.env.POSTGRES_PASSWORD,
+    'database': process.env.POSTGRES_DB,
+    'dialect': 'postgres',
+  },
+  'production': {
+    'username': process.env.POSTGRES_USER,
+    'password': process.env.POSTGRES_PASSWORD,
+    'database': process.env.POSTGRES_DB,
+    'dialect': 'postgres',
+  },
+};
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const sequelize = require('./models').sequelize;
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('..........SUCCESS_CONNECTION');
+  })
+  .catch(error => {
+    console.log('..........', error);
+  });
 
 require('dotenv');
 
@@ -8,5 +18,5 @@ const port = process.env.PORT || 3001;
 app.use(require('./routes'));
 
 app.listen(port, function () {
-  console.log(`App listen on ${port}!`)
+  console.log(`App listen on ${port}!`);
 });
