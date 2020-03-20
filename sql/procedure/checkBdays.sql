332c0cbed64e3e00671f6c5bbdc0ba287ad4bbda
Alexander Svinarev <shibanet0@gmail.com>
2020-03-20 16:55:52 +0300

    Added watcher to send a message

--- sql/procedure/checkBdays.sql
+++ sql/procedure/checkBdays.sql
@@ -1,18 +1,21 @@
 CREATE PROCEDURE "checkBdays" ()
 LANGUAGE SQL
 AS $$
-  INSERT INTO "BdaySchedules" ("bdayId", "date", "isCongratulate")
+  INSERT INTO "BdaySchedules" ("bdayId", "date", "isCongratulate", "createdAt", "updatedAt")
   SELECT
     "id" AS "bdayId",
     "date",
     FALSE,
+    CURRENT_DATE,
+    CURRENT_DATE
   FROM
     "Bdays"
   WHERE
-    "date" = CURRENT_DATE
+    EXTRACT(DAY FROM date) = EXTRACT(DAY FROM CURRENT_DATE)
   ON CONFLICT
     DO NOTHING;
 
   DELETE FROM "BdaySchedules"
-    WHERE "date" < CURRENT_DATE - interval '3 DAY';
+  WHERE EXTRACT(DAY FROM date) < EXTRACT(DAY FROM CURRENT_DATE - interval '3 DAY')
 $$;
+
--- watchers/index.js
+++ watchers/index.js
@@ -1,4 +1,38 @@
 const sql = require('../sql');
+const axios = require('axios');
+const { BdaySchedule, Bday } = require('../models');
+
+function checkTimeAndSendGreeting() {
+  const run = () => {
+    const utcHours = new Date().getHours() + new Date().getTimezoneOffset() / 60;
+    if ((utcHours < 0 ? 24 + utcHours : utcHours) >= 10) {
+      BdaySchedule.findAll({ where: { isCongratulate: false } }).then((records) => {
+        records.forEach((record) => {
+          Bday.findOne({ where: { id: record.dataValues.bdayId } }).then((bday) => {
+            console.table(bday.dataValues);
+
+            if (bday.data && bday.data.templateId) {
+              axios
+                .post('https://bday.shibanet0.tech/system/message', {
+                  channel: 'UU534SR1Q',
+                  text: 'string',
+                  blocks: [],
+                })
+                .then(() => {
+                  record.update({
+                    isCongratulate: true,
+                  });
+                });
+            }
+          });
+        });
+      });
+    }
+  };
+  run();
+
+  setInterval(run, 60 * 1000); /* 1 MINUTE */
+}
 
 function checkBday() {
   sql.procedure.checkBdays();
@@ -10,6 +44,7 @@ function checkBday() {
 function startAll() {
   new Promise(() => {
     checkBday();
+    checkTimeAndSendGreeting();
   });
 }
 
